c693e18ce53526eef603616f514c33afe2717122
valaskaomer <132208759+valaskaomer@users.noreply.github.com>
2025-05-25 13:54:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# Gerekli kütüphaneleri koda aktarmak
 from ultralytics import YOLO
 import cv2
 import time
 import torch
 import os
 
+# Model seçimi ve cihaz işlemleri
 assert os.path.exists("elSayilariYolo11nv2.pt"), "Model dosyası bulunamadı!"
 model = YOLO("elSayilariYolo11nv2.pt")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 print(f"Model is using device: {device}")
 
+# Kamera işlermleri
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
@@ -20,12 +23,14 @@ frame_count = 0
 fps_list = []
 last_result = None
 
+# Görüntü yakalanacak olan yer
 try:
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
+        # Fps hesaplama
         current_time = time.time()
         fps = 1 / (current_time - prev_time)
         prev_time = current_time
@@ -34,6 +39,7 @@ try:
             fps_list.pop(0)
         avg_fps = sum(fps_list) / len(fps_list)
 
+        # Yakalanan görüntü üzerindeki işlemler ve doğruluğun belirlnmesi
         if frame_count % predict_every_n_frames == 0:
             with torch.no_grad():
                 results = model.predict(source=frame, imgsz=640, conf=0.5, stream=True)
@@ -42,12 +48,15 @@ try:
         frame_count += 1
         annotated_frame = last_result.plot() if last_result else frame
 
+        # Fps gösterimi ve başlık gösterimi
         cv2.putText(annotated_frame, f"FPS: {int(avg_fps)}", (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow("YOLOv11 Ile El Sayilari", annotated_frame)
 
+        # q tuşuna basılana kadar açık kalsın
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+# Yapılan işlemleri ramden silmek
 finally:
     cap.release()
     cv2.destroyAllWindows()
